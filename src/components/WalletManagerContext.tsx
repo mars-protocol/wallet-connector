bb1e1810ca17799300d17b48fe14ef1fe2a8e849
Linkie Link <linkielink.dev@gmail.com>
2023-02-02 17:18:46 +0100

    v1.2.3: fixed the rpc issue

--- src/components/WalletManagerContext.tsx
+++ src/components/WalletManagerContext.tsx
@@ -26,7 +26,7 @@ const useWalletManager = () => {
 const useWallet = useShuttle
 
 const getClient = async (rpc: string) => {
-  const rpcURL = rpc.slice(-1) === "/" ? rpc : `/${rpc}`
+  const rpcURL = rpc.slice(-1) === "/" ? rpc : `${rpc}/`
   const queryClient = await CosmWasmClient.connect(rpcURL)
 
   return queryClient
